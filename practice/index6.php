9ec7ee3d34ff341a2129d9bc7f940099f14b452a
hanzlikhan <khangormani79@gmail.com>
2024-09-25 22:20:11 +0500

    init

---
+++ practice/index6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Declare two string variables
+  $firstName = "John";
+  $lastName = "Doe";
+
+  // Concatenate and print the full name
+  echo "Full Name: " . $firstName . " " . $lastName;
+  ?>
+
+</body>
+
+</html>
