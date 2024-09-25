9fc0c4c89e4ff5b338a5f5bc9074428b39385b08
hanzlikhan <khangormani79@gmail.com>
2024-09-25 22:20:06 +0500

    init

---
+++ practice/index4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+// Declare a variable
+$message = "Welcome to PHP scripting!";
+
+// Display the value using echo
+echo "Using echo: " . $message . "<br>";
+
+// Display the value using print
+print "Using print: " . $message;
+?>
+</body>
+</html>
