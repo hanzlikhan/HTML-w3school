cff2a3cd67d8df2149fef510a23dc149dd80fb58
hanzlikhan <khangormani79@gmail.com>
2024-09-25 22:20:01 +0500

    init

---
+++ practice/index5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Declare a floating-point variable
+  $price = 99.99;
+
+  // Display the floating-point value
+  echo "The price is: " . $price;
+  ?>
+</body>
+
+</html>
