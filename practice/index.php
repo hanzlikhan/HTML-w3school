b304160143c456d39395beedae40338f132c0db9
hanzlikhan <khangormani79@gmail.com>
2024-09-25 22:19:46 +0500

    init

---
+++ practice/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Declare a string variable containing your name
+  $name = "Muhammad Hanzla";
+
+  // Display the name variable
+  echo "Hello, my name is " . $name;
+  ?>
+
+</body>
+
+</html>
